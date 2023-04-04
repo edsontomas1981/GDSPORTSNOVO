881efb988570264eeb3c6a60f7cf413e447dd957
Edson <edsontomasdev@gmail.com>
2023-04-03 21:55:24 -0300

    Atualização

--- produtos/models/produtos.py
+++ produtos/models/produtos.py
@@ -1,6 +1,14 @@
 from django.db import models
+from produtos.models.categoria import Categoria
 from produtos.models.subcategoria import Subcategoria
 
-class Produtos(models.Model):
 
-    pass
+class Produtos(models.Model):
+    categoria_fk = models.ForeignKey(Categoria, on_delete=models.CASCADE, blank=False, null=True)
+    subcategoria_fk = models.ForeignKey(Subcategoria, on_delete=models.CASCADE, blank=False, null=True)
+    descricao = models.CharField(max_length=30, null=True)
+    marca = models.CharField(max_length=30, null=True)
+    preco = models.FloatField(null=True)
+    cor = models.CharField(max_length=30, null=True)
+    tamanho = models.CharField(max_length=30, null=True)
+    obs = models.CharField(max_length=30, null=True)
--- produtos/tests/tests_categorias.py
+++ produtos/tests/tests_categorias.py
@@ -6,27 +6,61 @@ class Categoria_test(TestCase):
     def setUp(self):
         self.categoria=Categoria()
         self.subcategoria=Subcategoria()
-                    
-    def test_calculaFrete(self):
-        dados={'categoria':"Brasileirão"}
-        self.assertEqual(self.categoria.create_categoria(dados),200)
-        self.assertEqual(self.categoria.read_categoria(1),200)
-        self.assertEqual(self.categoria.read_categoria(2),404)
-        dados={'categoria':"Europa"}
-        self.assertEqual(self.categoria.update_categoria(1,dados),200)
-        self.assertEqual(self.categoria.obj_categoria.categoria,"Europa")
-        dadoSub={'descricao':"Bayern",'categoria':self.categoria.obj_categoria}
-        self.assertEqual(self.subcategoria.create_subcategoria(dadoSub),200)
-        self.assertEqual(self.subcategoria.obj_subcategoria.descricao,"Bayern")
-        self.assertEqual(self.subcategoria.obj_subcategoria.categoria_fk.categoria,"Europa")
-        self.assertEqual(self.subcategoria.read_subcategoria(1),200)
-        
-        dadoSub={'descricao':"Bayern",'categoria':self.categoria.obj_categoria}
-        self.assertEqual(self.categoria.update_categoria(1,dados),200)
-        self.assertEqual(self.subcategoria.obj_subcategoria)
 
-        self.assertEqual(self.categoria.delete_categoria(1),200)
-        self.assertEqual(self.categoria.read_categoria(1),404)
+    def test_categoria(self):
+        dados = {'categoria':"Brasileirão"}
+        dados_erro = {}
+        '''teste de criação de categoria'''
+        self.assertEqual(self.categoria.create_categoria(dados), 200)
+        self.assertEqual(self.categoria.create_categoria(dados_erro), 300)
+
+        '''teste de read Categoria'''
+        self.assertEqual(self.categoria.read_categoria(1), 200)
+        self.assertEqual(self.categoria.read_categoria(2), 404)
+
+        dados = {'categoria': "Europa"}
+        '''teste de update'''
+        self.assertEqual(self.categoria.update_categoria(1, dados), 200)
+        self.assertEqual(self.categoria.update_categoria(1, dados_erro), 300)
+        self.assertEqual(self.categoria.obj_categoria.categoria, "Europa")
+        self.assertEqual(self.categoria.delete_categoria(1), 200)
+        self.assertEqual(self.categoria.read_categoria(1), 404)
+        # --------------------------------------------------------------------------------------------------------------
+        dados = {'categoria': "Brasileirão"}
+        self.assertEqual(self.categoria.create_categoria(dados), 200)
+        dados_sub = {'descricao': "Corinthians", 'categoria': self.categoria.obj_categoria}
+
+        '''Teste criar subcategoria'''
+        self.assertEqual(self.subcategoria.create_subcategoria(dados_sub), 200)
+        self.assertEqual(self.subcategoria.create_subcategoria(dados_erro), 300)
+
+        '''Teste buscar subcategoria'''
+        self.assertEqual(self.subcategoria.read_subcategoria(1), 200)
+        self.assertEqual(self.subcategoria.read_subcategoria(2), 404)
+        self.assertEqual(self.subcategoria.read_subcategoria(dados_erro), 300)
+        self.assertEqual(self.subcategoria.obj_subcategoria.categoria_fk.categoria, "Brasileirão")
+        self.assertEqual(self.subcategoria.obj_subcategoria.descricao, "Corinthians")
+
+        '''Carrega nova categoria para teste'''
+        dados = {'categoria': "Europa"}
+        self.assertEqual(self.categoria.create_categoria(dados), 200)
+        self.categoria.read_categoria(3)
+        dados_sub = {'descricao': "Bayern", 'categoria': self.categoria.obj_categoria}
+
+        '''Teste atualizar subcategoria'''
+        self.assertEqual(self.subcategoria.update_subcategoria(1, dados_sub), 200)
+        self.assertEqual(self.subcategoria.update_subcategoria(1, dados_erro), 300)
+        self.assertEqual(self.subcategoria.obj_subcategoria.descricao, "Bayern")
+
+        '''Teste delete subcategoria'''
+        self.assertEqual(self.subcategoria.delete_subcategoria(1), 200)
+        self.assertEqual(self.subcategoria.read_subcategoria(1), 404)
+
+
+
+
+
+
 
 
 
